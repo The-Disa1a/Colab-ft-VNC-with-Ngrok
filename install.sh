fdd599ed083754e3448aa25a8acffecb6a3f198c
Chamod Disala <thedisala@yahoo.com>
2025-03-03 11:04:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ setup_vnc
 wall_change
 
 #echo ngrok address
-curl -s http://127.0.0.1:4040/api/tunnels | grep -o '"tcp://[^"]*' | sed 's/tcp:\/\///'
+curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'tcp://[^"]*' | sed 's/tcp:\/\///; s/"//g'
 
 #loop
 start_time=$(date +%s); while true; do elapsed=$(( $(date +%s) - start_time )); elapsed_formatted=$(printf "%02d:%02d:%02d" $((elapsed/3600)) $(((elapsed%3600)/60)) $((elapsed%60))); echo -ne "\rRunning Time: $elapsed_formatted"; sleep 1; done
