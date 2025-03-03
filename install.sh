0125acd19d6e57e72e26c3d71616e4f8ee42c1ce
Chamod Disala <thedisala@yahoo.com>
2025-03-03 11:46:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,6 +57,8 @@ setup_vnc() {
     export DISPLAY=:1
     /usr/bin/autocutsel -fork
     /usr/bin/autocutsel -selection PRIMARY -fork
+    sed -i 's|^Exec=/usr/bin/google-chrome-stable.*|Exec=/usr/bin/google-chrome-stable --no-sandbox %U|' /usr/share/applications/google-chrome.desktop
+    update-desktop-database ~/.local/share/applications
     ngrok tcp --region in  5901 > /dev/null 2>&1 &
   
     echo "Setup completed."
