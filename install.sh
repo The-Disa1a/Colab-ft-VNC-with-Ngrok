c4c3219039af15833f3c119dd50000f8c5fbfbcb
Chamod Disala <thedisala@yahoo.com>
2025-03-09 10:55:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ setup_vnc() {
 CHROME_BACKUP_PATH="/content/drive/MyDrive/ChromeBackup.zip"
 NIGHTLY_BACKUP_PATH="/content/drive/MyDrive/Nightly.zip"
 CHROME_PROFILE="~/.config/google-chrome/Default"
-NIGHTLY_PROFILE="~/.cache/mozilla/firefox/rz9xaw27.discord.automate"
+NIGHTLY_PROFILE="~/.cache/mozilla/firefox"
 
 # Function to handle backup when script exits (Ctrl+C)
 backup_on_exit() {
