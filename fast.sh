4fe9b18123fa6c52fddb380455a768f12ba578e1
Chamod Disala <thedisala@yahoo.com>
2025-03-24 07:44:33 +0530

    Update and rename backup.sh to fast.sh

---
+++ fast.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install tigervnc-standalone-server -y
+echo "123456" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+vncserver :1
+ngrok config add-authtoken 2La5clGyFRFBCOsZaJRgg1Q9J9l_msXpseMtGPWn3n1oo3Cu
+ngrok tcp --region ap  5901 > /dev/null 2>&1 &
+sleep 5
+curl -s http://127.0.0.1:4040/api/tunnels | grep -o '"tcp://[^"]*' | sed 's/tcp:\/\///'
+start_time=$(date +%s); while true; do elapsed=$(( $(date +%s) - start_time )); elapsed_formatted=$(printf "%02d:%02d:%02d" $((elapsed/3600)) $(((elapsed%3600)/60)) $((elapsed%60))); echo -ne "\rRunning Time: $elapsed_formatted"; sleep 1; done
