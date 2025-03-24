ce36edb6600a2916ebafff261a5bb3a233b80591
Chamod Disala <thedisala@yahoo.com>
2025-03-24 08:04:49 +0530

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -14,8 +14,7 @@ echo "123456" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 vncserver :1
 ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
-ngrok tcp --region ap  5901 > /dev/null 2>&1 &
+ngrok tcp --region ap 5901 > /dev/null 2>&1 &
 sleep 5
-ngrok_addr=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'tcp://[^"]*' | sed 's/tcp:\/\///; s/"//g')
-echo "$ngrok_addr"
+curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'tcp://[^"]*' | sed 's/tcp:\/\///; s/"//g'
 start_time=$(date +%s); while true; do elapsed=$(( $(date +%s) - start_time )); elapsed_formatted=$(printf "%02d:%02d:%02d" $((elapsed/3600)) $(((elapsed%3600)/60)) $((elapsed%60))); echo -ne "\rRunning Time: $elapsed_formatted"; sleep 15; done
