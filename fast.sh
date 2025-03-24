54b80ea554f9b1b2081b5b2204f2ceb5999c6eca
Chamod Disala <thedisala@yahoo.com>
2025-03-24 07:58:00 +0530

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+# Check if an argument (Ngrok auth token) is provided
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 \"<NGROK_AUTH_TOKEN>\""
+    exit 1
+fi
+
+NGROK_AUTH_TOKEN="$1"
+
 sudo apt update
 sudo apt install tigervnc-standalone-server -y
 echo "123456" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 vncserver :1
-ngrok config add-authtoken 2P8v3I9oxTnCsnsVcKIOjd9Rt4Y_5jzoNH4hQQNfWcR8iQz8i
+ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
 ngrok tcp --region ap  5901 > /dev/null 2>&1 &
 sleep 5
-curl -s http://127.0.0.1:4040/api/tunnels | grep -o '"tcp://[^"]*' | sed 's/tcp:\/\///'
-start_time=$(date +%s); while true; do elapsed=$(( $(date +%s) - start_time )); elapsed_formatted=$(printf "%02d:%02d:%02d" $((elapsed/3600)) $(((elapsed%3600)/60)) $((elapsed%60))); echo -ne "\rRunning Time: $elapsed_formatted"; sleep 1; done
+ngrok_addr=$(curl -s http://127.0.0.1:4040/api/tunnels | grep -o 'tcp://[^"]*' | sed 's/tcp:\/\///; s/"//g')
+echo "$ngrok_addr"
+start_time=$(date +%s); while true; do elapsed=$(( $(date +%s) - start_time )); elapsed_formatted=$(printf "%02d:%02d:%02d" $((elapsed/3600)) $(((elapsed%3600)/60)) $((elapsed%60))); echo -ne "\rRunning Time: $elapsed_formatted"; sleep 15; done
