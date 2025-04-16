bb20a2ceb27b7cdc9b9ee8009cf86c7f0eb579f2
Chamod Disala <thedisala@yahoo.com>
2025-04-16 08:58:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -302,7 +302,7 @@ fi
 # Main loop: live running time updated on the same line and automatic backup every 5 minutes
 start_time=$(date +%s)
 last_backup_time=$(date +%s)
-backup_interval=10  # 3600 seconds = 60 minutes
+backup_interval=300  # 3600 seconds = 60 minutes
 
 while true; do
     current_time=$(date +%s)
